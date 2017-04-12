4bdff5950b4708dc3429f8697b4dfed44b01a876
Frederic TAIEB <frederic.taieb@gmail.com>
2017-04-12 13:26:32 +0200

    Receiver becomes a Thread

--- Receiver.py
+++ Receiver.py
@@ -2,40 +2,34 @@ import socket
 from Led import *
 from Sound import *
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
+class Receiver(threading.Thread):
+  ip = "127.0.0.1"
+  p = 5005
+  sock = None
 
-while True:
-  data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-  print "%s received by : %s" %(data, str(addr))
+  def __init__(self, ip, p):
+    threading.Thread.__init__(self)
+    self.ip = ip
+    self.p = p
+    self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    self.sock.bind((ip, p))
+
+  def run(self):
+    while True:
+      data, addr = self.sock.recvfrom(1024) # buffer size is 1024 bytes
+      print "%s received by : %s" %(data, str(addr))
   
-  #Led
-  if data[0:3] == "led":
-    gpio = data[4:6]
-    cmd = data[7]
-    param = data[9:11]
-    l = Led(gpio, cmd, param)
-    l.start()
-  else:
-    s = Sound()
-    s.start()
+      #Led
+      if data[0:3] == "led":
+        gpio = data[4:6]
+        cmd = data[7]
+        param = data[9:11]
+        l = Led(gpio, cmd, param)
+        l.start()
+      else:
+        s = Sound()
+        s.start()
 
-  '''
-  if data[0:3] == "led":
-    l = Led(int(data[3:5]))
-    if data[6:7] == "1":
-      l.led_on()
-    elif data[6:7] == "0":
-      l.led_off()
-    elif data[6:7] == "3":
-      l.led_blink(int(data[7:8]), float(data[8:9]))
-    else:
-      print "Unknown Command"
-  elif data[0:3] == "snd":
-    print "Son Recu"
-  else:
-    print "Unknown Command"
-  '''
+r = Receiver("192.168.77.3", 5005)
+r.start()
---
+++ Receiver_working.py
@@ -0,0 +1,41 @@
+import socket
+from Led import *
+from Sound import *
+
+UDP_IP = "192.168.77.3"
+UDP_PORT = 5005
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+  data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+  print "%s received by : %s" %(data, str(addr))
+  
+  #Led
+  if data[0:3] == "led":
+    gpio = data[4:6]
+    cmd = data[7]
+    param = data[9:11]
+    l = Led(gpio, cmd, param)
+    l.start()
+  else:
+    s = Sound()
+    s.start()
+
+  '''
+  if data[0:3] == "led":
+    l = Led(int(data[3:5]))
+    if data[6:7] == "1":
+      l.led_on()
+    elif data[6:7] == "0":
+      l.led_off()
+    elif data[6:7] == "3":
+      l.led_blink(int(data[7:8]), float(data[8:9]))
+    else:
+      print "Unknown Command"
+  elif data[0:3] == "snd":
+    print "Son Recu"
+  else:
+    print "Unknown Command"
+  '''
